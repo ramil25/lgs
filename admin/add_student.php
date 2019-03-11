1497495f404b42bdab62bfb88a708df921176a1e
mirasrobert <mrmirasrobert@gmail.com>
2019-03-11 22:51:34 +0800

    LAYOUT TEXBOXT ONLY ADD_STUDENT.PHP

--- admin/add_student.php
+++ admin/add_student.php
@@ -1,3 +1,6 @@
+<?php  
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,7 +21,8 @@
   <!-- endinject -->
   <link rel="shortcut icon" href="../images/lspu.png" />
 </head>
-
+<style type="text/css">
+</style>
 <body>
   <div class="container-scroller">
     <!-- partial:partials/_navbar.html -->
@@ -133,63 +137,121 @@
       <div class="main-panel">
         <div class="content-wrapper">
           <div class="row">
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 grid-margin"> 
-                      <form method="POST" class="card card-sm">
-        <div class="card-body row no-gutters align-items-center">
-          <input type="text" name="search" class="col" placeholder="Search..." name="search" required />
-           <button type="submit" name="submit-search" class="col-auto">
-           <i class="mdi mdi-magnify btn-success icon-sm"></i></button>
-           </div>
-          </form>
+                
+         
+            
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 grid-margin stretch-card">
+              <div class="card card-statistics">
+                <div class="card-body">
+                    <div class="float-left">
+                    	<div class="add-student">
+                       <form class="form" method="post" />
+          <label>Name: </label>
+          <input type="text" name="fullname" placeholder="Full Name" required /><br>
+          <label>Gender: </label>
+          <input type="text" name="gender" placeholder="Gender" required /><br>
+          <label>Last School Attended: </label> 
+          <input type="text" name="school_last_attended" placeholder="School Name" required  /><br>
+          <label>Strand / Course: </label>
+          <input type="text" name="strand_course" placeholder="Enter Student Strand" required /><br>
+          <label>GWA: </label>
+          <input type="text" name="grade_GWA" placeholder="Grade" required /><br>
+          <label>Math: </label>
+          <input type="text" name="grade_Math" placeholder="Grade" required /><br>
+          <label>English: </label>
+          <input type="text" name="grade_English" placeholder="Grade" required /><br>
+          <label>Science: </label>
+          <input type="text" name="grade_Science" placeholder="Grade" required /><br>
+          
+          <label>1st Choice: </label>
+          <input type="text" name="fchoice" placeholder="Course" required><br>
+          <label>2nd Choice: </label>
+          <input type="text" name="schoice" placeholder="Course" required><br>
+          <label>3rd Choice: </label>
+          <input type="text" name="tchoice" placeholder="Course" required><br>
+          <label>Raw Score: </label>
+          <input type="text" name="raw_score" placeholder="Input Score" required><br>
+          <label>Remarks: </label>
+          <input type="text" name="remarks" placeholder="Remarks Here" required><br>
+          <label>Date: </label><br>
+          </div>
+          <select name="year">
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+            <option value="2026">2026</option>
+            <option value="2027">2027</option>
+            <option value="2028">2028</option>
+            <option value="2029">2029</option>
+             <option value="2030">2030</option>
+          </select>
+       <select name="day">
+         <option value="1">1</option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         <option value="5">5</option>
+         <option value="6">6</option>
+         <option value="7">7</option>
+         <option value="8">8</option>
+         <option value="9">9</option>
+         <option value="10">10</option>
+         <option value="11">11</option>
+         <option value="12">12</option>
+         <option value="13">13</option>
+         <option value="14">14</option>
+         <option value="15">15</option>
+         <option value="16">16</option>
+         <option value="17">17</option>
+         <option value="18">18</option>
+         <option value="19">19</option>
+         <option value="20">20</option>
+         <option value="21">21</option>
+         <option value="22">22</option>
+         <option value="23">23</option>
+         <option value="24">24</option>
+         <option value="25">25</option>
+         <option value="26">26</option>
+         <option value="27">27</option>
+         <option value="28">28</option>
+         <option value="29">29</option>
+         <option value="30">30</option>
+         <option value="31">31</option>
+       </select>
+       </select>
+          <select name="month">
+            <option value="June">June</option>
+            <option value="July">July</option>
+            <option value="August">August</option>
+            <option value="September">September</option>
+            <option value="October">October</option>
+            <option value="November">November</option>
+            <option value="December">December</option>
+            <option value="January">January</option>
+            <option value="February">February</option>
+            <option value="March">March</option>
+            <option value="April">April</option>
+             <option value="May">May</option>
+          </select><br> 
+          <br><label>Upload File: </label>
+          <input type="file" name="photo_link" placeholder="Course" style="text-indent: 0px"><br>
+        <br>  
+          <button style="margin-top: -15px;" type="submit" name="add-std" class="btn btn-success">Add</button>
+        </form>
+                    
+                 
+                </div>
+                
+              </div>
             </div>
+  
         
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 grid-margin stretch-card">
-              <div class="card card-statistics">
-                  <a href="add_student.php">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-account-multiple-plus text-danger icon-lg"></i>
-                      <h1 class="float-right ml-5 mt-2">Add Student</h1>
-                    </div>
-                  </div>
-                </div>
-                </a>
-              </div>
-            </div>
-
-             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 grid-margin stretch-card">
-              <div class="card card-statistics">
-                  <a href="">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi  mdi-sync text-danger icon-lg"></i>
-                      <h1 class="float-right ml-5 mt-2">Update Student</h1>
-                    </div>
-                  </div>
-                </div>
-                </a>
-              </div>
-            </div>
-
-             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 grid-margin stretch-card">
-              <div class="card card-statistics">
-                  <a href="">
-                <div class="card-body">
-                  <div class="clearfix">
-                    <div class="float-left">
-                      <i class="mdi mdi-account-search text-danger icon-lg"></i>
-                      <h1 class="float-right ml-5 mt-2">View All</h1>
-                    </div>
-                  </div>
-                </div>
-                </a>
-              </div>
-            </div>
             
           </div>
-          
         </div>
         <!-- content-wrapper ends -->
         <!-- partial:partials/_footer.html -->
