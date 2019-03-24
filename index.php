a5adff4a54c2071af27f250cfb3636ec466d4651
itsmekobie123 <poging_kobie@yahoo.com>
2019-03-24 19:14:59 +0800

    I edited the layout for the index of students and lgs

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Students</title>
+	<title>Newsfeed</title>
 
 	<link rel="shortcut icon" href="images/lspu.png">
 	<link rel="stylesheet" href="css/students.css">
@@ -29,12 +29,13 @@
 			<img src="images/lspulogo.png" class="img-fluid" alt="Home" style="margin-left:10px;float:left;width: 120px; height: auto;"></a>
 
 			<nav>
-				<a href="index.php" style="margin-right: 30px;">Newsfeeds</a>
+				<a href="index.php"  class="selected" style="margin-right: 30px;">Newsfeeds</a>
 				<a href="result.php" style="margin-right: 30px;">Results</a>
+				<a href="req_moral.php" style="margin-right: 30px;">Request Good Moral</a>
 				<a href="aboutus.php" style="margin-right: 30px;">About Us</a>
 			</nav>
 
-			<a href="login.php" class="btn logout-button">Login</a>
+			<a href="logout.php" class="btn logout-button">Login</a>
 
 		</div>
 
@@ -45,21 +46,35 @@
 <!-- The content of your page would go here. -->
 
 
-<div class="menu" style="background-color: #eaf0f2;">
-	<h2> Dear LSPU student </h2>
-	<p> Are you looking for someone <br>
-		...you can trust and talk with? <br>
-		...who can listen to your problem? <br>
-		...whom you can confide your secrets? <br>
-		...whom you can discuss your plans and goals in life? <br><br>
-		or <br><br>
-		you're looing for a friend who can help you become what you really want to be yourself.<br>
-		Well, just drop by the Guidance Office anytime you need oour service during our councelling hours from Monday to Friday. <br>
-		The world is drifting ....</p>
+	<div class="container-fluid">
+	<div class="row">
+	<div class="col-sm-6 col-md-8 col-lg-8 ">
+	<div class="menu" style="background-color: #eaf0f2;">
+	<h2> LSPU Mission </h2>
+	<p> The Laguna State Polytechnic University is a center of Development transforming lives and communities.<br><br>
 
+	<h2> LSPU Vision </h2>
+	<p> LSPU provides quality education through responsive instruction, distinctive research, sustainable extension <br>and production services for improved quality of life towards nation building.<br><br>
 
-</div>
+	<h2> Guidance Mission </h2>
+	<p> The Guidance Office views a person as individual created in the image and likeness of God. An individual must be respected and recognized with worth and dignity according to their capabilities, abilities, skills and needs. The Guidance Services are designed to assist an individual person to become self-integrated, self-directed and self-reliant person acknowledge to his capabilities for his future achievements.<br><br>
 
+	<h2> Guidance Vision </h2>
+	<p> In a modern technology, a discipline skillful, we envision the Guidance Office as a student center for a robust, holistic person with conductive facilities and testing for professional training for total development of an individual personality through pyschological, counselling, assessment and emotional therapy.<br><br><br><br><br>	
+
+	</div>	
+	</div>
+
+	<div class="col-sm-6 col-md-4 col-lg-4 ">
+	<div class="menu" style="background-color: #eaf0f2;">	
+		<h4>Images</h4>
+	</div>
+	</div>
+
+	</div>
+	</div>
+
+	
 </body>
 
 </html>
