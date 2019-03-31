45f356e723f28207210853f294b8b4d17d38660e
itsmekobie123 <poging_kobie@yahoo.com>
2019-03-31 12:15:13 +0800

    newsfeed.php has now have a working function

--- students/index.php
+++ students/index.php
@@ -51,17 +51,28 @@
 	<div class="row">
 	<div class="col-sm-6 col-md-8 col-lg-8 ">
 	<div class="menu" style="background-color: #eaf0f2;">
-	<h2> LSPU Mission </h2>
-	<p> The Laguna State Polytechnic University is a center of Development transforming lives and communities.<br><br>
+	<center>
+	<table>
+		<tr>
+			<th style="text-align: center;">Announcements</th>
+		</tr>
+	</center>
+<?php
+	session_start();
+	require '../db.php';
 
-	<h2> LSPU Vision </h2>
-	<p> LSPU provides quality education through responsive instruction, distinctive research, sustainable extension <br>and production services for improved quality of life towards nation building.<br><br>
-
-	<h2> Guidance Mission </h2>
-	<p> The Guidance Office views a person as individual created in the image and likeness of God. An individual must be respected and recognized with worth and dignity according to their capabilities, abilities, skills and needs. The Guidance Services are designed to assist an individual person to become self-integrated, self-directed and self-reliant person acknowledge to his capabilities for his future achievements.<br><br>
-
-	<h2> Guidance Vision </h2>
-	<p> In a modern technology, a discipline skillful, we envision the Guidance Office as a student center for a robust, holistic person with conductive facilities and testing for professional training for total development of an individual personality through pyschological, counselling, assessment and emotional therapy.<br><br><br><br><br>	
+	$sql = "SELECT * FROM announcement";
+	$result = $conn -> query($sql);
+	if ($result -> num_rows > 0) {
+		while ($row = $result -> fetch_assoc()) {
+			echo "<tr><td>".$row["announcement"]."<br></td></tr>"; 
+		}
+		echo "</table>";
+	}
+	else {
+		echo "<i>0 results</i>";
+	}
+    ?>		
 
 	</div>	
 	</div>
