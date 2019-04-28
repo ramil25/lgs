19aa7480ae715e1d1c265498b64fea049c38ccb6
ramil25 <ramilkaharian25@gmail.com>
2019-04-28 19:12:22 +0800

    modified announcement database table

--- sql_table/announcement.sql
+++ sql_table/announcement.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2019 at 01:42 PM
+-- Generation Time: Apr 28, 2019 at 01:11 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `announcement` (
+  `user_id` int(200) NOT NULL,
   `announcement_id` int(11) NOT NULL,
   `announcement` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -37,9 +38,17 @@ CREATE TABLE `announcement` (
 -- Dumping data for table `announcement`
 --
 
-INSERT INTO `announcement` (`announcement_id`, `announcement`) VALUES
-(1, 'hello good morning LSPU'),
-(2, 'hello LSPU today is another day');
+INSERT INTO `announcement` (`user_id`, `announcement_id`, `announcement`) VALUES
+(18, 1, 'hello good morning LSPU'),
+(18, 2, 'hello LSPU today is another day'),
+(18, 3, 'Hello World hahahahahaha'),
+(18, 4, 'asasasasasasasasasasasasa'),
+(18, 5, 'zZZZXZcsfdgfhfhghgjkhkhkhkhgkhkhgkgkhgkhgkhgkh'),
+(18, 6, 'm                          nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn'),
+(18, 7, 'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'),
+(24, 8, 'ghfhfdghfhfhfhfhfhfhfh'),
+(18, 9, 'sassssssssssssssssssssssssssssssssss'),
+(24, 10, 'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk');
 
 --
 -- Indexes for dumped tables
@@ -59,7 +68,7 @@ ALTER TABLE `announcement`
 -- AUTO_INCREMENT for table `announcement`
 --
 ALTER TABLE `announcement`
-  MODIFY `announcement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `announcement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
