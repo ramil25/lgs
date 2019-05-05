3f3b3d35e17ff4521c44f6f52589140567b3e533
ramil25 <ramilkaharian25@gmail.com>
2019-05-05 12:53:34 +0800

    add security to students aboutus

--- students/aboutus.php
+++ students/aboutus.php
@@ -184,7 +184,7 @@ if ($_SESSION["user_level"]==2) {
 </html>
 <?php
 }
-else {
+else if($_SESSION["user_level"]!=2 || $_SESSION['username']=='') {
   echo '<div class="container-scroller">
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
@@ -195,5 +195,6 @@ else {
       </div>
   <H1 style="font-family:Arial;">PLEASE LOGIN <a href="/lgs/">HERE</a></H1>'
   ;
+  header('location: ../login.php');
 }
 ?>
